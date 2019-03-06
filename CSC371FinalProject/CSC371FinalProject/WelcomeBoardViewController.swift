cc31d836c77055a93b45050ce0211d1687f167d0
Nathaniel Alvarado <natea1014@outlook.com>
2019-03-06 16:11:26 -0600

    Image assets for Welcome Board

---
+++ CSC371FinalProject/CSC371FinalProject/WelcomeBoardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeBoardViewController.swift
+//  CSC371FinalProject
+//
+//  Created by Nathaniel Alvarado on 3/6/19.
+//  Copyright © 2019 Nathaniel Alvarado. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
