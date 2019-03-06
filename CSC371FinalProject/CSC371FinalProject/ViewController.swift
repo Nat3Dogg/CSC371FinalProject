2a1d85bd38cf489d8b6e6d62074b2a614e2b2d87
Nathaniel Alvarado <natea1014@outlook.com>
2019-03-06 14:55:40 -0600

    Initial commit

---
+++ CSC371FinalProject/CSC371FinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CSC371FinalProject
+//
+//  Created by Nathaniel Alvarado on 3/6/19.
+//  Copyright © 2019 Nathaniel Alvarado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
